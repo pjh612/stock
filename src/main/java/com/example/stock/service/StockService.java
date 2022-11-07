d9c8fd4c1e3d3ef0b3dbdb72d55a8293f88cd87f
pjh612 <pjh_jn@naver.com>
2022-11-07 18:23:48 +0900

    feat: Named락 재고 감소 기능 추가

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -1,6 +1,7 @@
 package com.example.stock.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.stock.domain.Stock;
@@ -43,6 +44,15 @@ public class StockService {
 	@Transactional
 	public void optimisticLockDecrease(Long id, Long quantity) {
 		Stock stock = stockRepository.findByIdWithOptimisticLock(id).orElseThrow();
+
+		stock.decrease(quantity);
+
+		stockRepository.saveAndFlush(stock);
+	}
+
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	public void namedLockDecrease(Long id, Long quantity) {
+		Stock stock = stockRepository.findById(id).orElseThrow();
 
 		stock.decrease(quantity);
 
